2cf72a6338e39caa5b89bdb9202bf1c0a49e05f9
Parth Gawade <gawadeparth777@gmail.com>
2026-02-04 20:58:30 +0530

    Fix leap year logic in leap_year.c

--- leap_year.c
+++ leap_year.c
@@ -3,11 +3,11 @@ int main() {
     int year;
     printf("enter a year:");
     scanf("%d",&year);
-    if (year%4==0) {
+    if ((year%400==0) || (year %4==0 && year%100 !=0)){
         printf("It is a leap year %d\n",year);
     }
     else {
-        printf("It is not a leap year");
+        printf("%d\n is not a leap year",year);
     }
     return 0;
 }
