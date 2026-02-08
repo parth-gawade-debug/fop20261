933e9807de1dea630fcedc2d5b864a331a837e78
parth-gawade-debug <gawadeparth777@gmail.com>
2026-02-08 03:59:05 +0000

    Add minutes_conversion.c to convert minutes into years and days

---
+++ basic_of_c_programming/minutes_conversion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int minutes_entered;
+	double years=0.0;
+	double days=0.0;
+	double minutes_in_year=0.0;
+	double minutes_in_day=0.0;
+	printf("enter the no. of minutes:\n ");
+	scanf("%d",& minutes_entered);
