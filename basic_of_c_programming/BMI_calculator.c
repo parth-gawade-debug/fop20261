ea6e7fbb9b43f90da7cb95ccd84eadadb6d99f3a
parth-gawade-debug <gawadeparth777@gmail.com>
2026-02-16 04:02:52 +0000

    Add BMI_calculator.c to calculate BMI based on weight and height

---
+++ basic_of_c_programming/BMI_calculator.c
@@ -0,0 +1,22 @@
+// this program calculates the BMI of a person based on their weight and height
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    float weight, height, bmi;
+    printf("Enter your weight in kilograms: ");
+    scanf("%f", &weight);
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+    bmi = weight / (height * height);
+    printf("Your BMI is: %.2f\n", bmi);
+    if (bmi < 18.5) {
+        printf("You are underweight.\n");
+    } else if (bmi >= 18.5 && bmi < 25) {
+        printf("You have a normal weight.\n");
+    } else if (bmi >= 25 && bmi < 30) {
+        printf("You are overweight.\n");
+    } else {
+        printf("You are obese.\n");
+    }
+    return 0;
+}
