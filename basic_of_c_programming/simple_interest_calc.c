aae645bd7c96ee6b1f57cdf1da811bf3885946f3
parth-gawade-debug <gawadeparth777@gmail.com>
2026-03-08 11:30:11 +0000

    simple program for calculating the simple interest

---
+++ basic_of_c_programming/simple_interest_calc.c
@@ -0,0 +1,14 @@
+// This program calculates simple interest based on user input for principal, rate, and time.
+#include <stdio.h>
+int main() {
+    float principal, rate, time, simpleInterest;
+    printf("Enter the principal amount: ");
+    scanf("%f", &principal);
+    printf("Enter the rate of interest (in percentage): ");
+    scanf("%f", &rate);
+    printf("Enter the time (in years): ");
+    scanf("%f", &time);
+    simpleInterest = (principal * rate * time) / 100;
+    printf("The simple interest is: %.2f\n", simpleInterest);
+    return 0;
+}
