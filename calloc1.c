e98b846ed4c8b96ea1a87b7fce5d7a352fdefa51
parth-gawade-debug <gawadeparth777@gmail.com>
2026-04-22 12:43:54 +0000

    took user input for array using calloc

--- calloc1.c
+++ calloc1.c
@@ -4,11 +4,9 @@
 int main() {
     int *ptr;
     ptr=(int *)calloc(5,sizeof(int)); // allocate memory for 5 integers
-    ptr[0] = 1; // assign values to the array
-    ptr[1] = 2;
-    ptr[2] = 65;
-    ptr[3] = 56;
-    ptr[4] = 5;
+    for(int i=0; i<5;i++) {
+        scanf("%d",&ptr[i]); // read values into the array
+    }
     for (int i = 0; i < 5; i++) {
         printf("%d\n",ptr[i]); // print the values of the array
     }
